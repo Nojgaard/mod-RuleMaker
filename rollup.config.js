5fd53a190801436e43bdcb4dcd40448ef3df8ee5
Nikolai Nøjgaard <nojgaard@imada.sdu.dk>
2020-08-12 15:04:12 +0200

    bundler up

---
+++ rollup.config.js
@@ -0,0 +1,40 @@
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
+
+const VERSION = process.env.VERSION || 'snapshot'; // default snapshot
+// const SOURCEMAPS = process.env.SOURCEMAPS === 'true'; // default false
+
+const input = './src/index.js';
+
+const name = 'MODviz';
+
+const configs = [
+  {
+    input,
+    output: {
+      file: 'build/MODviz.umd.js',
+      format: 'umd',
+      name,
+      sourcemap: 'inline'
+    },
+    plugins: [
+      nodeResolve(),
+      commonjs({ include: '**/node_modules/**' })
+    ]
+  },
+
+  {
+    input,
+    output: {
+      file: 'build/MODviz.min.js',
+      format: 'umd',
+      name
+    },
+    plugins: [
+      nodeResolve(),
+      commonjs({ include: '**/node_modules/**' })
+    ]
+  }
+]
+
+export default configs
